bd8fe448766759b0930700496d955bdcf350bf4e
talel kaabachi <talelcool@gmail.com>
2026-01-20 11:02:51 +0100

    chore(lint): ignore local cache directory

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-if [ -z "$GIT_LFS_ENABLED" ]; then
-  command git-lfs "$@"
-else
-  command git-lfs "$@"
+#!/usr/bin/env sh
+if [ -z "$HUSKY" ]; then
+  export HUSKY=1
+fi
+
+hook_name=$(basename "$0")
+if [ -f "$(dirname "$0")/$hook_name" ]; then
+  . "$(dirname "$0")/$hook_name" "$@"
 fi
